c9765b30343397eea5385c9b53e5fc1dd94848eb
Umut <umtgokdemir@gmail.com>
2024-03-09 11:00:51 +0100

    Gauss Legendre algorithm implemented.

--- pi_estimate_MC.py
+++ pi_estimate_MC.py
@@ -36,7 +36,25 @@ def Nilakantha_Series(attempts):
         n = (i+1)*2
         estimation += 4*(-1)**i/n/(n+1)/(n+2)
     return (estimation)
+
+def Gauss_Legendre(attempts):
+    #Initialize values
+    a, b, t, p = 1, 1/math.sqrt(2), 1/4, 1
+    #Define Arithmetic Geometric Means
+    a_next = lambda a,b: (a+b)/2
+    b_next = lambda a,b: math.sqrt(a*b)
+    t_next = lambda a,a_next,p,t: t-p*(a-a_next)**2
+    #Calculate variables at the desired iteration
+    for _ in range(attempts):
+        a_n1 = a_next(a,b)
+        b = b_next(a,b)
+        t = t_next(a,a_n1,p,t)
+        p = p*2
+        a = a_n1
+    #Return estimation
+    return ((a+b)**2)/4/t
 # Run estimator 1,000,000 times to get more precise result
 print(MC_PI_calculator(1000000))
-print(Leibniz_Formula(1000000))
-print(Nilakantha_Series(1000))
+print(Leibniz_Formula(100))
+print(Nilakantha_Series(100))
+print(Gauss_Legendre(10))

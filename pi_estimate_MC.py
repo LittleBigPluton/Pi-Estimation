b7401705382548bb91b65c4785a0cf70327b5744
Umut <umtgokdemir@gmail.com>
2024-03-09 01:45:36 +0100

    Leibniz method added

--- pi_estimate_MC.py
+++ pi_estimate_MC.py
@@ -20,5 +20,15 @@ def MC_PI_calculator (attempts):
             inside_circle += 1
     return(inside_circle/attempts*4)
 
+def Leibniz_Formula(attempts):
+    estimation = 0
+    for k in range(attempts):
+        divider = 1/(2*k+1)
+        if k%2 == 0:
+            estimation += divider
+        else:
+            estimation -= divider
+    return 4*estimation
 # Run estimator 1,000,000 times to get more precise result
 print(MC_PI_calculator(1000000))
+print(Leibniz_Formula(1000000))
